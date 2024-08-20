00845b814245e6abb832227543cd7736751fae4e
Adam Dalwai <adamdalwai1@gmail.com>
2024-08-19 20:59:27 -0400

    changed header style import

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from 'react';
-import styles from './Header.module.css';
+import styles from './header.module.css';
 import { signOut, useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
 import '../css/styles.css';
